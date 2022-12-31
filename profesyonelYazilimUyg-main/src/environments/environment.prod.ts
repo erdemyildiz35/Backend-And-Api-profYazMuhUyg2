0fa06bdbd52bb28b9598be8bb3b5349ac9449cec
BerkeKocabalkan <berke.kocabalkan@gmail.com>
2022-12-31 19:53:45 +0300

    File Upload

---
+++ profesyonelYazilimUyg-main/src/environments/environment.prod.ts
@@ -0,0 +1,16 @@
+const environment = {
+  production: true,
+  adminEmail: "info@cinarturan.com",
+  api_key: "AIzaSyDyFVtGZ7hvlhLIO4p8GkleoHU51EFV5EA",
+  database_url: "https://ng-shopapp-d4ef5-default-rtdb.firebaseio.com/"
+};
+
+const mongoEnvironment = {
+  connectionString:'mongodb://localhost:27017',
+  dbName:'angular'
+};
+
+export { environment, mongoEnvironment };
+
+// development
+// production
---
+++ profesyonelYazilimUyg-main/src/src/environments/environment.prod.ts
@@ -0,0 +1,10 @@
+export const environment = {
+  production: true,
+  adminEmail: "info@cinarturan.com",
+  api_key: "AIzaSyDyFVtGZ7hvlhLIO4p8GkleoHU51EFV5EA",
+  database_url: "https://ng-shopapp-d4ef5-default-rtdb.firebaseio.com/"
+};
+
+
+// development
+// production
