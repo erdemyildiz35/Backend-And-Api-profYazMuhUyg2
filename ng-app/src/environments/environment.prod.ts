c9550c2587138829cfd6d9cefecbeee092d057ce
erdemyildiz35 <90454576+erdemyildiz35@users.noreply.github.com>
2023-01-03 16:17:14 +0300

    son hali

---
+++ ng-app/src/environments/environment.prod.ts
@@ -0,0 +1,10 @@
+export const environment = {
+  production: true,
+  adminEmail: "erdemyildiz356299@outlook.com",
+  api_key: "AIzaSyDyFVtGZ7hvlhLIO4p8GkleoHU51EFV5EA",
+  database_url: "https://ng-backend-e3b0e-default-rtdb.firebaseio.com/"
+};
+
+
+// development
+// production
--- profesyonelYazilimUyg-main/src/app/products/product.model.ts
+++
@@ -1,9 +0,0 @@
-export interface Product {
-    id: any;
-    name: string;
-    price: number;
-    imageUrl: string;
-    description: string;
-    isActive: boolean;
-    categoryId: number
-}
